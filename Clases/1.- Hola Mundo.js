7fb356f229f0e923a0e25616714c31fb53053321
Diego Castro <diegoalbertocastropastor@gmail.com>
2024-07-16 18:39:19 -0500

    Update 1.- Hola Mundo.js

--- Clases/1.- Hola Mundo.js
+++ Clases/1.- Hola Mundo.js
@@ -2,7 +2,8 @@
 Para imprimir cosas, mensajes o lo que se desee en la consola se debe usar la siguiente sintaxis:*/
 console.log("Hola mundo");
 // Este código mostrará 'Hola mundo' en tu consola.
+-----------------------------------------------------
 /*Comentarios: Los comentarios son una forma de documentar tu código para guiarte y llevar un orden o si quieres trabajar en equipo.
-Se inician con: "// " y lo que escribas por delante de ello no se ejecutará ni se mostrará en la consola. Si quieres hacer comentarios de más de una línea usa:
-lo que esté entre los asteriscos será un comentario.
+Se inician con: "// " y lo que escribas por delante de ello no se ejecutará ni se mostrará en la consola. Si quieres hacer comentarios de más de una línea usa: "/ * * /"
+lo que esté entre los asteriscos será un comentario, pero no pongas espacios entre el slash y el asterisco, es solo referencial.
 Con esto ya sabrás como ejecutar cualquier ejercicio que se te pida en la terminal.*/
