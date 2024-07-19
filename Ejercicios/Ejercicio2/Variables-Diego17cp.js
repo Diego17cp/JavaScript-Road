3d0163165290acdcd8bf83f7e68993f36450d5a5
Diego17 <diegoalbertocastropastor@gmail.com>
2024-07-19 00:21:24 -0500

    Resolvi el ejercicio 2

---
+++ Ejercicios/Ejercicio2/Variables-Diego17cp.js
@@ -0,0 +1,21 @@
+// Ejercicios - Clase 2
+  // 1-
+    const name="Diego";
+    let edad1=18;
+    const pais="Perú";
+    console.log("Me llamo "+name+", tengo "+edad1+" y soy de "+pais);
+// 2- 
+    const nombre="Diego";
+    let edad2=18;
+    const nacionalidad="peruano";
+    let fecha = new Date("2024-07-19");
+    let carrera="Ing. Software";
+    console.log("Soy "+nombre+" tengo "+edad2+" años. Soy "+nacionalidad+" y estoy en la carrera de "+carrera+". Estoy haciendo este ejercicio el día "+fecha);
+// 3-
+    let telf="942111345";
+    telf=942111345;
+    let ID=1123;
+    ID="1123";
+    let edad_bro="11";
+    edad_bro=11;
+    console.log("Mi hermano tiene "+edad_bro+" años y su teléfono es "+telf+" y su ID es "+ID);
