17b3917bff624ec21af557d1662b6d3e378c4ffc
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-19 17:01:05 -0500

    Create Variables-JulianB0315.js

---
+++ Ejercicios/Ejercicio2/Variables-JulianB0315.js
@@ -0,0 +1,23 @@
+//1
+//Define 3 variables e imprímelas en la consola una por una o concatenalas.
+    let nombre_1="Julian";
+    let edad_1=19;
+    let pais="Peru";
+    console.log("Mi nombre es "+nombre_1+", tengo "+edad_2_+" años y soy de "+pais);
+//2 
+// Define 5 variables y concatenalas para mostrar una frase con sentido
+    let nombre_2="Pepe";
+    let edad_2=18;
+    let nacionalidad="Chileno";
+    let fecha = new Date("2023-03-19");
+    let carrera="Zapatero nuclear";
+    console.log("Soy "+nombre_2+" tengo "+edad_2+" años. Soy "+nacionalidad+" y estoy en la carrera de "+carrera+". Estoy haciendo este ejercicio el día "+fecha);
+//3
+//Define 3 variables de un tipo y posteriormente cambialas a otro tipo de dato y muéstralas formando una oración o mensaje
+    let Dni="78822900";
+    Dni=78822900;
+    let edad=30;
+    edad="30";
+    let telefono="987456123";
+    telefono=987456123;
+    console.log("El de DNI de mi mamá es"+Dni+" tiene "+edad+" años y su telefono es "+telefono);
